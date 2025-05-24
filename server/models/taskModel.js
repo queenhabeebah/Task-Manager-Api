b1b4760cd35dc24b120e2ae42736c897720e6d65
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-05-24 12:54:34 +0300

    Set up .env, db.js, server.js and taskModel.js

---
+++ server/models/taskModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, `Task title is required`],
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+    },
+    priority: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        default: 'medium'
+    },
+    dueDate: {
+        type: Date
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+   timestamps: true 
+})
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task
