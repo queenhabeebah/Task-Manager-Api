7516fb490546039a79c6679ce4d140ef85634598
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-05-25 15:30:07 +0300

    Create taskController with CRUD funtions

---
+++ server/controllers/taskController.js
@@ -0,0 +1,74 @@
+const Task = require("../models/taskModel")
+
+// To get all tasks
+const getTasks = async (req, res) =>{
+    try{
+        const tasks = await Task.find().sort({ createdAt: -1})
+        res.json(tasks)
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+// To create a task
+const createTask = async (req, res) =>{
+    try{
+        const task = new Task(req.body)
+        const savedTask = await task.save()
+        res.status(201).json(savedTask)
+    } catch (error){
+        res.status(400).json({ message: error.message})
+    }
+}
+
+// To get a single task
+const getTask = async (req, res) => {
+    try{
+        const task = await Task.findById(req.params.id)
+        if(!task){
+            return res.status(404).json({ message: "Task not found"})
+        }
+        res.json(task)
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+// To update a task
+const updateTask = async (req, res) =>{
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if(!task){
+            return res.status(404).json({ message: 'Task not found'})
+        }
+        res.json(task)
+    } catch(error){
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+// To delete a task
+const deleteTask = async(req, res) => {
+    try{
+const task = await Task.findByIdAndDelete(req.params.id)
+if(!task){
+    return res.status(404).json({ message: 'Task not found'})
+}
+res.json({message: 'Task deleted successfully'})
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {
+    getTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+}
