734c90a3b3087715e491cd441fcaa5110ef5de69
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-05-25 15:55:05 +0300

    Update server.js with routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const taskRoutes = require('./routes/taskRoutes')
 
 dotenv.config()
 connectDB()
@@ -15,7 +16,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-// app.use('/api/tasks', require('./routes/taskRoutes'))
+app.use('/api/tasks', taskRoutes)
 
 const  PORT = process.env.PORT || 5000
 
